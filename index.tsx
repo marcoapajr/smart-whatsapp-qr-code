bedfcd90ef21d90040bf5450c6bcfcd90e0ebff9
marcoapajr <marquinhoaurelio@gmail.com>
2025-11-19 23:23:56 -0300

    google adsense

--- index.tsx
+++ index.tsx
@@ -114,8 +114,8 @@ const AdUnit = () => {
        */}
        <ins className="adsbygoogle block w-full text-center"
             style={{ display: 'block' }}
-            data-ad-client="ca-pub-XXXXXXXXXXXXXXXX" 
-            data-ad-slot="1234567890"
+            data-ad-client="ca-pub-3492462368285464" 
+            data-ad-slot="13019457659"
             data-ad-format="auto"
             data-full-width-responsive="true"></ins>
     </div>
